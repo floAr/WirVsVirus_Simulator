0c20729a06c5f4f14f41cb05f9d505ea98a0f60e
Florian Uhde <floar@gmx.de>
2020-03-21 10:42:36 +0100

    add webgl starting point

---
+++ Assets/Plugins/WebBridge.jslib
@@ -0,0 +1,6 @@
+mergeInto(LibraryManager.library, {
+
+  PushJsonData: function (str_json){
+	 window.alert(JSON.parse(Pointer_stringify(str)));
+  }
+});
---
+++ Assets/Systems/WebBridge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class WebBridge : MonoBehaviour
+{
+    // see https://docs.unity3d.com/Manual/webgl-interactingwithbrowserscripting.html
+
+
+
+
+    [DllImport("__Internal")]
+    private static extern void PushJsonData(string str);
+
+
+    void Start()
+    {
+  
+    }
+}
+
